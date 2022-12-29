12d73cdea8f928f54fc33fb6712835890531c534
Artur Krystopchuk <arturkrys@gmail.com>
2022-12-29 21:47:29 +0000

    change of vectors to arrays

--- project.cpp
+++ project.cpp
@@ -19,27 +19,23 @@ typedef struct edge
     int weight;
 } Edge;
 
-/* Global vectors */
-
-vector<Vertice> vertices;
-vector<Edge> edges;
-
 /* Functions*/
 
-void makeSet(int V)
+void makeSet(int V, Vertice vertices[])
 {
     vertices[V].parent = V;
     vertices[V].rank = 0;
 }
 
-int findSet(int V)
+int findSet(int V, Vertice vertices[])
 {
     if (vertices[V].parent != V)
-        vertices[V].parent = findSet(vertices[V].parent);
+        vertices[V]
+            .parent = findSet(vertices[V].parent, vertices);
     return vertices[V].parent;
 }
 
-void link(int v1, int v2)
+void link(int v1, int v2, Vertice vertices[])
 {
     if (vertices[v1].rank > vertices[v2].rank)
         vertices[v2].parent = v1;
@@ -51,9 +47,9 @@ void link(int v1, int v2)
     }
 }
 
-void treeUnion(int v1, int v2)
+void treeUnion(int v1, int v2, Vertice vertices[])
 {
-    link(findSet(v1), findSet(v2));
+    link(findSet(v1, vertices), findSet(v2, vertices), vertices);
 }
 
 bool weightComparator(Edge e1, Edge e2)
@@ -63,20 +59,22 @@ bool weightComparator(Edge e1, Edge e2)
 
 // Performs Kruskal's algorithm but from heavist to lightest, and instead of
 // adding edges to the MST, it simply adds their weight to "sum"
-int adaptedKruskal()
+int adaptedKruskal(Edge edges[], Vertice vertices[], int numEdges)
 {
     int sum = 0;
 
     // Sorts all edges from heaviest to lightest
-    std::sort(edges.begin(), edges.end(), weightComparator);
+    std::sort(edges, edges + numEdges, weightComparator);
 
-    for (Edge e : edges)
-        if (findSet(e.vertice1) != findSet(e.vertice2))
+    for (int i = 0; i < numEdges; i++)
+    {
+        Edge edge = edges[i];
+        if (findSet(edge.vertice1, vertices) != findSet(edge.vertice2, vertices))
         {
-            sum += e.weight;
-            treeUnion(e.vertice1, e.vertice2);
+            sum += edge.weight;
+            treeUnion(edge.vertice1, edge.vertice2, vertices);
         }
-
+    }
     return sum;
 }
 
@@ -87,14 +85,13 @@ int main()
     {
         exit(1);
     }
-
-    vertices.resize(numVertices);
-    edges.resize(numEdges);
+    Vertice vertices[numVertices];
+    Edge edges[numEdges];
 
     // Performs "makeSet" on every vertice.
     // The vertice "1" is on vertices[0], "2" is on vertices[1], etc.
-    for (int i = 0; i < numVertices; i++)
-        makeSet(i);
+    for (int i = 0; i <= numVertices; i++)
+        makeSet(i, vertices);
 
     for (int i = 0; i < numEdges; i++)
     {
@@ -109,7 +106,7 @@ int main()
         edges[i] = e;
     }
 
-    printf("%d\n", adaptedKruskal());
+    printf("%d\n", adaptedKruskal(edges, vertices, numEdges));
 
     return 0;
 }
